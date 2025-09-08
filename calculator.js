625fd5b37b5d977501a817505599eb01c4fe0d15
RaihanAlFairdaus <hanrillhn505@gmail.com>
2025-09-08 14:30:19 +0700

    Delete calculator.js

--- calculator.js
+++
@@ -1,7 +0,0 @@
-function calculate() {
-  const input = document.getElementById('calcInput').value;
-  let result;
-  try { result = eval(input); }
-  catch(e) { result = 'Error'; }
-  document.getElementById('calcResult').textContent = 'Hasil: ' + result;
-}
