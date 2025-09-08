d5e9a5ff2d3dba0dff742fb624c4d98507234747
RaihanAlFairdaus <hanrillhn505@gmail.com>
2025-09-08 14:29:19 +0700

    Delete translate.js

--- translate.js
+++
@@ -1,6 +0,0 @@
-function translateText() {
-  const text = prompt('Masukkan teks yang ingin diterjemahkan:');
-  if(!text) return;
-  // Placeholder: nanti bisa pakai API Google Translate
-  alert('Teks diterjemahkan (sementara placeholder): ' + text.split('').reverse().join(''));
-}
